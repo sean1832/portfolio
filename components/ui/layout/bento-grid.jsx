ad1adbde504cec781a703bb82a34163e93712f89
Zeke Zhang <sean1832725142@gmail.com>
2024-04-21 16:17:26 +1000

    style: adjust bento box description font colour

--- components/ui/layout/bento-grid.jsx
+++ components/ui/layout/bento-grid.jsx
@@ -17,10 +17,8 @@ export const BentoGridItem = ({ className, title, description, header, icon }) =
 
       <div className="group-hover/bento:translate-x-2 transition duration-200">
         {icon}
-        <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
-          {title}
-        </div>
-        <div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">
+        <div className="font-sans font-bold mb-2 mt-2">{title}</div>
+        <div className="font-sans font-normal text-xs text-gray-600 dark:text-gray-300">
           {description}
         </div>
       </div>
