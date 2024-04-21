5d04c4e260157635bb2b8411611ba6d26fd0c110
Zeke Zhang <sean1832725142@gmail.com>
2024-04-21 12:32:47 +1000

    feat: add icon to webpage

--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,12 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Zeke Zhang",
   description: "Portfolio of Zeke Zhang",
+  icons: {
+    icon: ["/favicon.ico?v=4"],
+    apple: ["/apple-touch-icon.png?v=4"],
+    shortcut: ["/apple-touch-icon.png"],
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({ children }) {
