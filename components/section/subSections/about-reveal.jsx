8dac63c1e3c811195da3970f00ad80de3156b190
Zeke Zhang <sean1832725142@gmail.com>
2024-04-21 22:35:04 +1000

    try: change src to dynamic

--- components/section/subSections/about-reveal.jsx
+++ components/section/subSections/about-reveal.jsx
@@ -4,20 +4,14 @@ import { buttonVariants } from "@/components/ui/buttons/button";
 import { GitHubLogoIcon, InstagramLogoIcon } from "@radix-ui/react-icons";
 import { IoLogoYoutube } from "react-icons/io5";
 import Image from "next/image";
-import profileImage from "@/public/profile/zeke.webp";
 import Link from "next/link";
 
 const AboutReveal = () => {
-  const variants = {
-    hidden: { opacity: 0, y: 20 },
-    visible: { opacity: 1, y: 0 },
-    visibleDelayed: { opacity: 1, y: 0, transition: { delay: 0.5, duration: 1 } },
-  };
   return (
     <div className=" grid grid-cols-1 md:grid-cols-2 items-center gap-4">
       <div className="max-w-2xl w-full flex justify-center">
         <Image
-          src={profileImage}
+          src={profile.image.src}
           alt={profile.image.alt}
           style={{ width: "auto", objectFit: "cover", objectPosition: "bottom" }}
           className="rounded-xl"
