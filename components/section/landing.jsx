cf745511dfaae48fb4b964d07e944a41f30cad50
Zeke Zhang <sean1832725142@gmail.com>
2024-04-21 17:12:40 +1000

    feat: change landing page title and subtitle text

--- components/section/landing.jsx
+++ components/section/landing.jsx
@@ -12,11 +12,17 @@ const TextDecodeCycle = dynamic(() => import("@/components/ui/animation/text-dec
 const Landing = () => {
   return (
     <section className="flex flex-col h-screen items-center justify-center">
-      <div className="text-4xl sm:text-6xl m:text-9xl lg:text-9xl font-bold text-center">
-        <TextDecode text="ZEKE ZHANG" interval={50} />
+      <div className="text-4xl sm:text-6xl m:text-6xl lg:text-6xl font-bold text-center flex flex-col gap-0">
+        <TextDecode text="INTELEGENT SYNTHESIS" interval={50} />
       </div>
       <TextDecodeCycle
-        texts={["INTELLEGENT FORMATION", "MATERIAL REUSE", "DIGITAL FABRICATION"]}
+        texts={[
+          "ALGORITHMIC MORPHOGENESIS",
+          "AUTONOMOUS MATERIAL RECONSTRUCTION",
+          "COGNITIVE ARCHITECTURAL MACHINES",
+          "LOW-TECH ASSEMBLY CRAFT",
+          "DIFFUSION TECTONICS",
+        ]}
         interval={50}
         cycleInterval={3000}
       />
