15d9006acbf48f6891dca562f6118e4b9c08576e
Zeke Zhang <sean1832725142@gmail.com>
2024-04-21 16:10:26 +1000

    style: change footer layout

--- components/section/footer.jsx
+++ components/section/footer.jsx
@@ -3,9 +3,9 @@ import { Separator } from "../ui/separator";
 
 const Footer = () => {
   return (
-    <div id="footer" className="py-5">
+    <div id="footer" className="flex flex-col pt-10">
       <Separator />
-      <footer className="flex items-center justify-center py-5">
+      <footer className="flex items-center justify-center py-10">
         <p className="text-gray-600 dark:text-gray-400 text-sm uppercase">
           copyright &copy; 2024 ZekeZhang.com - All rights reserved.
         </p>
