cb779b6c4a24ea5e2abd70b8a48638aa499a1950
Zeke Zhang <sean1832725142@gmail.com>
2024-04-21 21:45:06 +1000

    feat: re-implement scroll reveal with framer motion

--- components/section/about.jsx
+++ components/section/about.jsx
@@ -6,33 +6,45 @@ import { GitHubLogoIcon, InstagramLogoIcon } from "@radix-ui/react-icons";
 import { IoLogoYoutube } from "react-icons/io5";
 import Link from "next/link";
 import Image from "next/image";
-import { RevealList } from "next-reveal";
+import { motion } from "framer-motion";
 import profileImage from "@/public/profile/zeke.webp";
 
 const About = () => {
+  const variants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0 },
+    visibleDelayed: { opacity: 1, y: 0, transition: { delay: 0.5, duration: 1 } },
+  };
   return (
     <div id="about">
       <section className="flex flex-col justify-center items-center h-screen w-full">
-        <RevealList
-          origin="bottom"
-          opacity={0}
-          delay={200}
-          interval={300}
-          duration={1500}
-          className={` grid grid-cols-1 md:grid-cols-2 items-center gap-4`}
-        >
-          <div className="max-w-2xl w-full flex justify-center">
+        <div className=" grid grid-cols-1 md:grid-cols-2 items-center gap-4">
+          <motion.div
+            className="max-w-2xl w-full flex justify-center"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            transition={{ duration: 0.8 }}
+            variants={variants}
+          >
             <Image
               src={profileImage}
               alt={profile.image.alt}
               style={{ width: "auto", objectFit: "cover", objectPosition: "bottom" }}
               className="rounded-xl"
+              placeholder="blur"
               width={400}
               height={350}
-              placeholder="blur"
             />
-          </div>
-          <div className="max-w-2xl w-full px-4 flex flex-col gap-3">
+          </motion.div>
+          <motion.div
+            className="max-w-2xl w-full px-4 flex flex-col gap-3"
+            initial="hidden"
+            whileInView="visibleDelayed"
+            viewport={{ once: true }}
+            transition={{ duration: 0.8 }}
+            variants={variants}
+          >
             <h2 className="text-4xl font-bold">ZEKE ZHANG</h2>
             <p>{profile.about}</p>
             <div className="py-4 gap-9 flex justify-left">
@@ -58,8 +70,8 @@ const About = () => {
                 <IoLogoYoutube />
               </Link>
             </div>
-          </div>
-        </RevealList>
+          </motion.div>
+        </div>
       </section>
     </div>
   );
