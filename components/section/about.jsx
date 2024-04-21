3c8e9479140b445c1d7f040207ebe03d527fcb8f
Zeke Zhang <sean1832725142@gmail.com>
2024-04-21 21:59:07 +1000

    revert back to original due to vercel deployment failed

--- components/section/about.jsx
+++ components/section/about.jsx
@@ -1,76 +1,44 @@
-"use client";
 import React from "react";
 import profile from "@/data/profile.json";
 import { buttonVariants } from "../ui/buttons/button";
 import { GitHubLogoIcon, InstagramLogoIcon } from "@radix-ui/react-icons";
 import { IoLogoYoutube } from "react-icons/io5";
 import Link from "next/link";
-import Image from "next/image";
-import { motion } from "framer-motion";
-import profileImage from "@/public/profile/zeke.webp";
+import BlurImage from "../ui/blur";
 
 const About = () => {
-  const variants = {
-    hidden: { opacity: 0, y: 20 },
-    visible: { opacity: 1, y: 0 },
-    visibleDelayed: { opacity: 1, y: 0, transition: { delay: 0.5, duration: 1 } },
-  };
   return (
     <div id="about">
       <section className="flex flex-col justify-center items-center h-screen w-full">
         <div className=" grid grid-cols-1 md:grid-cols-2 items-center gap-4">
-          <motion.div
-            className="max-w-2xl w-full flex justify-center"
-            initial="hidden"
-            whileInView="visible"
-            viewport={{ once: true }}
-            transition={{ duration: 0.8 }}
-            variants={variants}
-          >
-            <Image
-              src={profileImage}
+          <div className="max-w-2xl w-full flex justify-center">
+            <BlurImage
+              src={profile.image.src}
               alt={profile.image.alt}
               style={{ width: "auto", objectFit: "cover", objectPosition: "bottom" }}
               className="rounded-xl"
-              placeholder="blur"
               width={400}
               height={350}
             />
-          </motion.div>
-          <motion.div
-            className="max-w-2xl w-full px-4 flex flex-col gap-3"
-            initial="hidden"
-            whileInView="visibleDelayed"
-            viewport={{ once: true }}
-            transition={{ duration: 0.8 }}
-            variants={variants}
-          >
+          </div>
+          <div className="max-w-2xl w-full px-4 flex flex-col gap-3">
             <h2 className="text-4xl font-bold">ZEKE ZHANG</h2>
             <p>{profile.about}</p>
             <div className="py-4 gap-9 flex justify-left">
-              <Link
-                href={profile.social.github}
-                className={buttonVariants({ variant: "ghost" })}
-                target="_blank"
-              >
+              <Link href={profile.social.github} className={buttonVariants({ variant: "ghost" })}>
                 <GitHubLogoIcon />
               </Link>
               <Link
                 href={profile.social.instagram}
                 className={buttonVariants({ variant: "ghost" })}
-                target="_blank"
               >
                 <InstagramLogoIcon />
               </Link>
-              <Link
-                href={profile.social.youtube}
-                className={buttonVariants({ variant: "ghost" })}
-                target="_blank"
-              >
+              <Link href={profile.social.youtube} className={buttonVariants({ variant: "ghost" })}>
                 <IoLogoYoutube />
               </Link>
             </div>
-          </motion.div>
+          </div>
         </div>
       </section>
     </div>
