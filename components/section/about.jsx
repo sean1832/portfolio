273a8329b685e6e33f70f299ac6998b7bb7a8bf7
Zeke Zhang <sean1832725142@gmail.com>
2024-04-21 22:11:48 +1000

    try: wrap reveal

--- components/section/about.jsx
+++ components/section/about.jsx
@@ -1,45 +1,11 @@
 import React from "react";
-import profile from "@/data/profile.json";
-import { buttonVariants } from "../ui/buttons/button";
-import { GitHubLogoIcon, InstagramLogoIcon } from "@radix-ui/react-icons";
-import { IoLogoYoutube } from "react-icons/io5";
-import Link from "next/link";
-import BlurImage from "../ui/blur";
+import AboutReveal from "./subSections/about-reveal";
 
 const About = () => {
   return (
     <div id="about">
       <section className="flex flex-col justify-center items-center h-screen w-full">
-        <div className=" grid grid-cols-1 md:grid-cols-2 items-center gap-4">
-          <div className="max-w-2xl w-full flex justify-center">
-            <BlurImage
-              src={profile.image.src}
-              alt={profile.image.alt}
-              style={{ width: "auto", objectFit: "cover", objectPosition: "bottom" }}
-              className="rounded-xl"
-              width={400}
-              height={350}
-            />
-          </div>
-          <div className="max-w-2xl w-full px-4 flex flex-col gap-3">
-            <h2 className="text-4xl font-bold">ZEKE ZHANG</h2>
-            <p>{profile.about}</p>
-            <div className="py-4 gap-9 flex justify-left">
-              <Link href={profile.social.github} className={buttonVariants({ variant: "ghost" })}>
-                <GitHubLogoIcon />
-              </Link>
-              <Link
-                href={profile.social.instagram}
-                className={buttonVariants({ variant: "ghost" })}
-              >
-                <InstagramLogoIcon />
-              </Link>
-              <Link href={profile.social.youtube} className={buttonVariants({ variant: "ghost" })}>
-                <IoLogoYoutube />
-              </Link>
-            </div>
-          </div>
-        </div>
+        <AboutReveal />
       </section>
     </div>
   );
---
+++ components/section/subSections/about-reveal.jsx
@@ -0,0 +1,76 @@
+"use client";
+import React from "react";
+import profile from "@/data/profile.json";
+import { buttonVariants } from "@/components/ui/buttons/button";
+import { GitHubLogoIcon, InstagramLogoIcon } from "@radix-ui/react-icons";
+import { IoLogoYoutube } from "react-icons/io5";
+import { motion } from "framer-motion";
+import Image from "next/image";
+import profileImage from "@/public/profile/zeke.webp";
+import Link from "next/link";
+
+const AboutReveal = () => {
+  const variants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0 },
+    visibleDelayed: { opacity: 1, y: 0, transition: { delay: 0.5, duration: 1 } },
+  };
+  return (
+    <div className=" grid grid-cols-1 md:grid-cols-2 items-center gap-4">
+      <motion.div
+        className="max-w-2xl w-full flex justify-center"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+        transition={{ duration: 0.8 }}
+        variants={variants}
+      >
+        <Image
+          src={profileImage}
+          alt={profile.image.alt}
+          style={{ width: "auto", objectFit: "cover", objectPosition: "bottom" }}
+          className="rounded-xl"
+          placeholder="blur"
+          width={400}
+          height={350}
+        />
+      </motion.div>
+      <motion.div
+        className="max-w-2xl w-full px-4 flex flex-col gap-3"
+        initial="hidden"
+        whileInView="visibleDelayed"
+        viewport={{ once: true }}
+        transition={{ duration: 0.8 }}
+        variants={variants}
+      >
+        <h2 className="text-4xl font-bold">ZEKE ZHANG</h2>
+        <p>{profile.about}</p>
+        <div className="py-4 gap-9 flex justify-left">
+          <Link
+            href={profile.social.github}
+            className={buttonVariants({ variant: "ghost" })}
+            target="_blank"
+          >
+            <GitHubLogoIcon />
+          </Link>
+          <Link
+            href={profile.social.instagram}
+            className={buttonVariants({ variant: "ghost" })}
+            target="_blank"
+          >
+            <InstagramLogoIcon />
+          </Link>
+          <Link
+            href={profile.social.youtube}
+            className={buttonVariants({ variant: "ghost" })}
+            target="_blank"
+          >
+            <IoLogoYoutube />
+          </Link>
+        </div>
+      </motion.div>
+    </div>
+  );
+};
+
+export default AboutReveal;
