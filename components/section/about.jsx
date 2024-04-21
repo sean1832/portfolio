7d6ce0bdc6b4adbf5d24e14a3fb1dcf32ef71ccf
Zeke Zhang <sean1832725142@gmail.com>
2024-04-21 21:40:02 +1000

    feat: re-implement scroll reveal

--- components/section/about.jsx
+++ components/section/about.jsx
@@ -1,45 +1,65 @@
+"use client";
 import React from "react";
 import profile from "@/data/profile.json";
 import { buttonVariants } from "../ui/buttons/button";
 import { GitHubLogoIcon, InstagramLogoIcon } from "@radix-ui/react-icons";
 import { IoLogoYoutube } from "react-icons/io5";
 import Link from "next/link";
-import BlurImage from "../ui/blur";
+import Image from "next/image";
+import { RevealList } from "next-reveal";
+import profileImage from "@/public/profile/zeke.webp";
 
 const About = () => {
   return (
     <div id="about">
       <section className="flex flex-col justify-center items-center h-screen w-full">
-        <div className=" grid grid-cols-1 md:grid-cols-2 items-center gap-4">
+        <RevealList
+          origin="bottom"
+          opacity={0}
+          delay={200}
+          interval={300}
+          duration={1500}
+          className={` grid grid-cols-1 md:grid-cols-2 items-center gap-4`}
+        >
           <div className="max-w-2xl w-full flex justify-center">
-            <BlurImage
-              src={profile.image.src}
+            <Image
+              src={profileImage}
               alt={profile.image.alt}
               style={{ width: "auto", objectFit: "cover", objectPosition: "bottom" }}
               className="rounded-xl"
               width={400}
               height={350}
+              placeholder="blur"
             />
           </div>
           <div className="max-w-2xl w-full px-4 flex flex-col gap-3">
             <h2 className="text-4xl font-bold">ZEKE ZHANG</h2>
             <p>{profile.about}</p>
             <div className="py-4 gap-9 flex justify-left">
-              <Link href={profile.social.github} className={buttonVariants({ variant: "ghost" })}>
+              <Link
+                href={profile.social.github}
+                className={buttonVariants({ variant: "ghost" })}
+                target="_blank"
+              >
                 <GitHubLogoIcon />
               </Link>
               <Link
                 href={profile.social.instagram}
                 className={buttonVariants({ variant: "ghost" })}
+                target="_blank"
               >
                 <InstagramLogoIcon />
               </Link>
-              <Link href={profile.social.youtube} className={buttonVariants({ variant: "ghost" })}>
+              <Link
+                href={profile.social.youtube}
+                className={buttonVariants({ variant: "ghost" })}
+                target="_blank"
+              >
                 <IoLogoYoutube />
               </Link>
             </div>
           </div>
-        </div>
+        </RevealList>
       </section>
     </div>
   );
