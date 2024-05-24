0bf78fac992bb9338616fd797c3c34052c2766bf
Zeke Zhang <sean1832725142@gmail.com>
2024-05-24 19:14:12 +1000

    feat: add source code link to footer

--- components/section/footer.jsx
+++ components/section/footer.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import { Separator } from "../ui/separator";
+import ExternalTextLink from "../ui/external-text-link";
 
 const Footer = () => {
   return (
     <div id="footer" className="flex flex-col pt-10">
       <Separator />
-      <footer className="flex items-center justify-center py-10">
+      <footer className="flex flex-col items-center justify-center py-10">
         <p className="text-gray-600 dark:text-gray-400 text-sm uppercase text-center">
           copyright &copy; 2024 ZekeZhang.com - All rights reserved.
         </p>
+        {/* Source code Links */}
+        <div className="flex items-center space-x-4">
+          <ExternalTextLink
+            href="https://github.com/sean1832/zeke-zhang-portfolio"
+            className={`text-gray-600 dark:text-gray-400 text-sm uppercase text-center`}
+          >
+            Source Code
+          </ExternalTextLink>
+        </div>
       </footer>
     </div>
   );
