43fd52a0005d431f0c01517edfd1a2ed8ac4f190
Zeke Zhang <sean1832725142@gmail.com>
2024-05-25 16:14:02 +1000

    feat: remove link-preview for external link
    remove link-preview for external link due to unnecessary performance hit and many site does not have proper favicon and description.

--- components/ui/external-text-link.jsx
+++ components/ui/external-text-link.jsx
@@ -2,29 +2,20 @@ import React from "react";
 import Link from "next/link";
 import { ExternalLinkIcon } from "@radix-ui/react-icons";
 import { cn } from "@/lib/utils";
-import { HoverCard, HoverCardTrigger, HoverCardContent } from "./hover-card";
-import LinkPreview from "./link-preview";
 
 const ExternalTextLink = ({ children, href, key, className }) => {
   return (
-    <HoverCard>
-      <HoverCardTrigger asChild>
-        <Link target="_blank" href={href} key={key}>
-          <span
-            className={cn(
-              "underline inline-block items-center hover:text-primary hover:no-underline",
-              className
-            )}
-          >
-            {children}
-            <ExternalLinkIcon className="ml-1 w-4 h-4 inline-block" />
-          </span>
-        </Link>
-      </HoverCardTrigger>
-      <HoverCardContent className="w-[620px] text-sm">
-        <LinkPreview url={href} />
-      </HoverCardContent>
-    </HoverCard>
+    <Link target="_blank" href={href} key={key}>
+      <span
+        className={cn(
+          "underline inline-block items-center hover:text-primary hover:no-underline",
+          className
+        )}
+      >
+        {children}
+        <ExternalLinkIcon className="ml-1 w-4 h-4 inline-block" />
+      </span>
+    </Link>
   );
 };
 
