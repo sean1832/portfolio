2ea60b6c85275f98ddf0eb50f94a0e2bf26ef13b
Zeke Zhang <sean1832725142@gmail.com>
2024-05-25 11:56:09 +1000

    chore: add clickable image to all image
    - deprecate clickable-image.jsx
    - use `<a>` tag for all images

--- components/section/project-info.jsx
+++ components/section/project-info.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ExternalTextLink from "../ui/external-text-link";
 import ImageLists from "../ui/images-list";
-import ClickableImage from "../ui/clickable-image";
 import { YoutubeVideo } from "../ui/youtube-video";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { ExclamationTriangleIcon } from "@radix-ui/react-icons";
+import BlurImage from "../ui/blur";
 
 const ProjectInfo = ({ data }) => {
   const heroImage = data.images.find((image) => image.hero == true);
@@ -30,7 +30,14 @@ const ProjectInfo = ({ data }) => {
             data.video ? (
               <YoutubeVideo src={data.video.src} alt={data.video.alt} mute />
             ) : (
-              <ClickableImage src={heroImage.src} alt={heroImage.alt} />
+              <a href={heroImage.src} target="_blank">
+                <BlurImage
+                  src={heroImage.src}
+                  alt={heroImage.alt}
+                  fill
+                  style={{ objectFit: "cover" }}
+                />
+              </a>
             )
           }
         </div>
