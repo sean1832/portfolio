f6b52e0114ad08b732ff551fe240a63aa0a3e2cb
Zeke Zhang <sean1832725142@gmail.com>
2024-05-26 14:18:27 +1000

    feat: add title fade out for page transition

--- components/ui/animation/transition.jsx
+++ components/ui/animation/transition.jsx
@@ -1,7 +1,22 @@
 "use client";
 import { motion, AnimatePresence } from "framer-motion";
+import { useState, useEffect } from "react";
+import { usePathname } from "next/navigation";
+import projectData from "@/data/projects";
+
+const titles = projectData.reduce((acc, project) => {
+  acc[project.href] = project.name;
+  return acc;
+}, {});
 
 const Transition = ({ children }) => {
+  const pathname = usePathname();
+  const [pageTitle, setPageTitle] = useState("");
+
+  useEffect(() => {
+    setPageTitle(titles[pathname] || null);
+  }, [pathname]);
+
   const animateUp = {
     initial: {
       y: 0,
@@ -39,6 +54,16 @@ const Transition = ({ children }) => {
   return (
     <>
       <AnimatePresence mode="wait">
+        {pageTitle && (
+          <motion.h1
+            className="text-4xl dark:text-black text-white z-[60] fixed text-center flex justify-center items-center w-screen h-screen"
+            initial={{ opacity: 1 }}
+            animate={{ opacity: 0, transitionEnd: { display: "none" } }}
+            transition={{ duration: 2 }}
+          >
+            {pageTitle.toUpperCase()}
+          </motion.h1>
+        )}
         <motion.div
           key={`banner-1`}
           className="h-1/2 top-0 dark:bg-primary bg-black z-50 fixed w-screen"
