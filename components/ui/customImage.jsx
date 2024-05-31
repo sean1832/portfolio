79e6cf5bce1954fb298816ea7a489cdc9a067bbd
Zeke Zhang <sean1832725142@gmail.com>
2024-05-31 15:57:02 +1000

    fix: body video layout

--- components/ui/customImage.jsx
+++ components/ui/customImage.jsx
@@ -7,8 +7,8 @@ import { YoutubeVideo } from "./youtube-video";
 const CustomImage = ({ image }) => {
   if (image.isVideo) {
     return (
-      <div className={cn("relative w-full col-span-2", image.className)}>
-        <YoutubeVideo src={image.src} alt={image.alt} className={image.className} />
+      <div className={cn("relative w-full col-span-2 h-[300px] md:h-auto", image.className)}>
+        <YoutubeVideo src={image.src} alt={image.alt} />
       </div>
     );
   }
