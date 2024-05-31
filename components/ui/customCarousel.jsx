66dbc34d864f32ad9bee51badf0229e5b8998b7d
Zeke Zhang <sean1832725142@gmail.com>
2024-05-31 19:28:58 +1000

    feat: implement thumbnail gallery

--- components/ui/customCarousel.jsx
+++ components/ui/customCarousel.jsx
@@ -1,53 +1,10 @@
-"use client";
 import * as React from "react";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "../ui/carousel";
-import Autoplay from "embla-carousel-autoplay";
-import Image from "next/image";
-import ExternalTextLink from "../ui/external-text-link";
+import Gallery from "./gallery";
 
-const CustomCarousel = ({ data, className }) => {
+const CustomCarousel = ({ data }) => {
   const carouselImages = data.images.filter((image) => image.isCarousel);
-  const plugin = React.useRef(Autoplay({ delay: 4000, stopOnInteraction: true }));
   if (carouselImages.length === 0) return null;
-  return (
-    <Carousel className={className} plugins={[plugin.current]}>
-      <CarouselContent>
-        {carouselImages.map((image, i) => (
-          <CarouselItem key={i}>
-            <div className="flex flex-col items-center">
-              <Image
-                key={i}
-                src={image.src}
-                alt={image.alt}
-                className="aspect-video object-cover"
-                width={1920}
-                height={1080}
-              />
-              <p className="text-sm mt-2.5">{image.caption}</p>
-              {image.credit && (
-                <>
-                  <p className="text-sm mt-2.5 text-gray-500">
-                    Credit:{" "}
-                    <ExternalTextLink href={image.credit.url}>{image.credit.text}</ExternalTextLink>
-                  </p>
-                </>
-              )}
-            </div>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <div className="hidden lg:block">
-        <CarouselPrevious />
-        <CarouselNext />
-      </div>
-    </Carousel>
-  );
+  return <Gallery images={carouselImages} />;
 };
 
 export default CustomCarousel;
---
+++ components/ui/gallery.jsx
@@ -0,0 +1,113 @@
+"use client";
+import { useEffect, useState, useMemo, useRef } from "react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "../ui/carousel";
+import Image from "next/image";
+import Autoplay from "embla-carousel-autoplay";
+
+// Reference
+// https://www.reddit.com/r/nextjs/comments/1cgktu9/shadcnui_image_carousel_with_thumbnail_images/
+const Gallery = ({ images }) => {
+  const [mainApi, setMainApi] = useState(null);
+  const [thumbnailApi, setThumbnailApi] = useState(null);
+  const [current, setCurrent] = useState(0);
+
+  const mainImage = useMemo(
+    () =>
+      images.map((image, index) => (
+        <CarouselItem key={index} className="relative w-full h-[600px]">
+          <Image
+            src={image.src}
+            alt={`Carousel Main Image ${index + 1}`}
+            fill
+            style={{ objectFit: "contain" }}
+          />
+        </CarouselItem>
+      )),
+    [images]
+  );
+
+  const thumbnailImages = useMemo(
+    () =>
+      images.map((image, index) => (
+        <CarouselItem
+          key={index}
+          className="relative aspect-square w-full md:basis-1/6 basis-1/4"
+          onClick={() => handleClick(index)}
+        >
+          <Image
+            className={`${index === current ? "" : "opacity-70 hover:opacity-90"} p-1`}
+            src={image.src}
+            fill
+            alt={`Carousel Thumbnail Image ${index + 1}`}
+            style={{ objectFit: "cover" }}
+          />
+        </CarouselItem>
+      )),
+    [images, current]
+  );
+
+  useEffect(() => {
+    if (!mainApi || !thumbnailApi) {
+      return;
+    }
+
+    const handleTopSelect = () => {
+      const selected = mainApi.selectedScrollSnap();
+      setCurrent(selected);
+      thumbnailApi.scrollTo(selected);
+    };
+
+    const handleBottomSelect = () => {
+      const selected = thumbnailApi.selectedScrollSnap();
+      setCurrent(selected);
+      mainApi.scrollTo(selected);
+    };
+
+    mainApi.on("select", handleTopSelect);
+    thumbnailApi.on("select", handleBottomSelect);
+
+    return () => {
+      mainApi.off("select", handleTopSelect);
+      thumbnailApi.off("select", handleBottomSelect);
+    };
+  }, [mainApi, thumbnailApi]);
+
+  const handleClick = (index) => {
+    if (!mainApi || !thumbnailApi) {
+      return;
+    }
+    thumbnailApi.scrollTo(index);
+    mainApi.scrollTo(index);
+    setCurrent(index);
+  };
+
+  const plugin = useRef(Autoplay({ delay: 2000, stopOnInteraction: true }));
+
+  return (
+    <div className="py-4">
+      <Carousel
+        setApi={setMainApi}
+        plugins={[plugin.current]}
+        onMouseEnter={plugin.current.stop}
+        onMouseLeave={plugin.current.reset}
+      >
+        <CarouselContent className="m-1">{mainImage}</CarouselContent>
+      </Carousel>
+      <Carousel setApi={setThumbnailApi}>
+        <CarouselContent className="m-1">{thumbnailImages}</CarouselContent>
+        <div className="hidden lg:block">
+          <CarouselPrevious />
+          <CarouselNext />
+        </div>
+      </Carousel>
+    </div>
+  );
+};
+
+export default Gallery;
