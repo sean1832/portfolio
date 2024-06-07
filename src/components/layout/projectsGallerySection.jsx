fd369b8aa7449f5c1085ce1c400e6607cd95ce4c
Zeke Zhang <sean1832725142@gmail.com>
2024-06-08 03:34:02 +1000

    refactor: remove redundant <div> wrapping around ProjectsGallerySection <section> tag

--- src/components/layout/projectsGallerySection.jsx
+++ src/components/layout/projectsGallerySection.jsx
@@ -6,43 +6,39 @@ import BlurImage from "../ui/media/blur";
 
 const ProjectsGallerySection = async () => {
   return (
-    <div id="projects">
-      <section>
-        <BentoGrid className={`auto-rows-[30rem] md:auto-rows-[50rem] lg:grid-cols-6`}>
-          {projects.map((project, i) => {
-            let heroImage = null;
-            try {
-              if (!project.images) {
-                throw new Error(`Project "${project.name}" does not have images property.`);
-              }
-              heroImage = project.images.find((image) => image.isHero);
-              if (!heroImage) {
-                throw new Error(`Project "${project.name}" does not have a hero image.`);
-              }
-            } catch (error) {
-              console.error(error.message);
-              // You can also render a fallback UI here if needed
-              return (
-                <div key={i} className="text-red-500 font-bold">
-                  {error.message}
-                </div>
-              );
+    <section id="projects">
+      <BentoGrid className={`auto-rows-[30rem] md:auto-rows-[50rem] lg:grid-cols-6`}>
+        {projects.map((project, i) => {
+          let heroImage = null;
+          try {
+            if (!project.images) {
+              throw new Error(`Project "${project.name}" does not have images property.`);
             }
+            heroImage = project.images.find((image) => image.isHero);
+            if (!heroImage) {
+              throw new Error(`Project "${project.name}" does not have a hero image.`);
+            }
+          } catch (error) {
+            console.error(error.message);
+            // You can also render a fallback UI here if needed
             return (
-              <BentoGridItem
-                key={i}
-                title={project.name}
-                description={project.description}
-                header={
-                  <ImageHeader src={heroImage?.src} alt={heroImage?.alt} href={project.href} />
-                }
-                className={`${project.bentoAttributes.className} uppercase `}
-              />
+              <div key={i} className="text-red-500 font-bold">
+                {error.message}
+              </div>
             );
-          })}
-        </BentoGrid>
-      </section>
-    </div>
+          }
+          return (
+            <BentoGridItem
+              key={i}
+              title={project.name}
+              description={project.description}
+              header={<ImageHeader src={heroImage?.src} alt={heroImage?.alt} href={project.href} />}
+              className={`${project.bentoAttributes.className} uppercase `}
+            />
+          );
+        })}
+      </BentoGrid>
+    </section>
   );
 };
 
