7d9f22318f1fe77a7c718cf0028b3614698ac7d3
Zeke Zhang <sean1832725142@gmail.com>
2024-06-08 03:29:02 +1000

    refactor: remove isExternal condition check for blur image in CustomImage.jsx
    - isExternal check to be handled within blurImage function.

--- src/components/ui/media/customImage.jsx
+++ src/components/ui/media/customImage.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Image from "next/image";
 import BlurImage from "./blur";
 import { cn } from "@/utils/cn";
 
@@ -7,23 +6,14 @@ const CustomImage = ({ image }) => {
   if (image.isAdaptive) {
     return (
       <div className={cn("relative w-full col-span-2", image.className)}>
-        {image.isExternal ? (
-          <Image
-            src={image.src}
-            alt={image.alt}
-            style={{ objectFit: "contain", width: "100%", height: "100%" }}
-            width={image.width || 1920}
-            height={image.height || 1080}
-          />
-        ) : (
-          <BlurImage
-            src={image.src}
-            alt={image.alt}
-            style={{ objectFit: "cover", width: "100%", height: "auto" }}
-            width={image.width || 1920}
-            height={image.height || 1080}
-          />
-        )}
+        <BlurImage
+          src={image.src}
+          alt={image.alt}
+          style={{ objectFit: "cover", width: "100%", height: "auto" }}
+          width={image.width || 1920}
+          height={image.height || 1080}
+          isExternal={image.isExternal}
+        />
       </div>
     );
   } else {
@@ -31,21 +21,13 @@ const CustomImage = ({ image }) => {
       <div
         className={cn("relative h-[300px] md:h-[600px] md:col-span-2 col-span-2", image.className)}
       >
-        {image.isExternal ? (
-          <Image
-            src={image.src}
-            alt={image.alt}
-            className={cn("object-cover object-center w-full h-full", image.className)}
-            fill
-          />
-        ) : (
-          <BlurImage
-            src={image.src}
-            alt={image.alt}
-            className={cn("object-cover object-center w-full h-full", image.className)}
-            fill
-          />
-        )}
+        <BlurImage
+          src={image.src}
+          alt={image.alt}
+          className={cn("object-cover object-center w-full h-full", image.className)}
+          fill
+          isExternal={image.isExternal}
+        />
       </div>
     );
   }
