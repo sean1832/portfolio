ab09233ff02db89d3939d8633665552df6d16cc3
Zeke Zhang <sean1832725142@gmail.com>
2024-06-25 00:13:06 +1000

    :sparkles: feat(footer): fetch GitHub info like latest version, release URL, and repo URL from API

--- src/components/layout/footer.jsx
+++ src/components/layout/footer.jsx
@@ -2,30 +2,37 @@ import React from "react";
 import { Separator } from "../ui/separator";
 import ExternalTextLink from "../ui/external-text-link";
 import manifest from "@/data/manifest";
+import { FetchLatestUrl, FetchLatestVersion, FetchRepoUrl } from "@/lib/fetchGithub";
 
-const Footer = () => {
+const Footer = async () => {
+  const repo = manifest.repository.id;
+  const versionNum = await FetchLatestVersion(repo);
+  const repoUrl = await FetchRepoUrl(repo);
+  const latestUrl = await FetchLatestUrl(repo);
+
   return (
     <div id="footer" className="flex flex-col pt-10">
       <Separator />
       <footer className="flex flex-col items-center justify-center py-10">
         <p className="text-gray-600 dark:text-gray-400 text-sm uppercase text-center">
-          copyright &copy; 2024 ZekeZhang.com - All rights reserved.
+          copyright &copy; {new Date().getFullYear()} {manifest.copyright.name} - All rights
+          reserved.
         </p>
         {/* Source code Links */}
         <div className="flex items-center space-x-4">
           <ExternalTextLink
-            href={manifest.repository.url}
-            className={`text-gray-600 dark:text-gray-400 text-sm uppercase text-center`}
+            href={repoUrl}
+            className="text-gray-600 dark:text-gray-400 text-sm uppercase text-center"
           >
             Source Code
           </ExternalTextLink>
 
           {/* Version */}
           <ExternalTextLink
-            href={manifest.repository.latestRelease}
-            className={`text-gray-600 dark:text-gray-400 text-sm uppercase text-center`}
+            href={latestUrl}
+            className="text-gray-600 dark:text-gray-400 text-sm uppercase text-center"
           >
-            Version: {manifest.version}
+            Version: {versionNum}
           </ExternalTextLink>
         </div>
       </footer>
