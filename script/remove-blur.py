65f74cd65aa35ba68a1915817405a3f7e0a76480
Zeke Zhang <sean1832725142@gmail.com>
2024-06-27 20:13:21 +1000

    :zap: perf(build): add blur data URL generator script to improve build speed

---
+++ script/remove-blur.py
@@ -0,0 +1,21 @@
+import json
+def read_json(json_path):
+  with open(json_path, 'r') as f:
+    return json.load(f)
+  
+def write_json(json_path, data):
+  with open(json_path, 'w') as f:
+    json.dump(data, f, indent=2)
+
+
+if __name__ == '__main__':
+  data = read_json("src/data/projects.json")
+  key = "blurDataURL"
+
+  for project in data:
+    for image in project["media"]:
+      if key in image:
+        print(f"removing from: {image['src']}")
+        del image[key]
+  write_json("src/data/projects.json", data)
+  print("\n\ndone!")
