73fe85bd44cf375a13b71c7bba808e00a0e8c40b
Zeke Zhang <sean1832725142@gmail.com>
2024-06-29 15:52:30 +1000

    :zap: perf(image): priorities `avif`

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 import withPlaiceholder from "@plaiceholder/next";
 const nextConfig = {
   images: {
+    formats: ["image/avif", "image/webp"],
     remotePatterns: [
       {
         protocol: "https",
