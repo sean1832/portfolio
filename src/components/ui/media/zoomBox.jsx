b19c9156387f2af6524ec292561b4c9a36e72752
Zeke Zhang <sean1832725142@gmail.com>
2024-06-29 18:45:09 +1000

    :sparkles: feat(zoombox): zoom to full window width rather than intrisic width

--- src/components/ui/media/zoomBox.jsx
+++ src/components/ui/media/zoomBox.jsx
@@ -27,7 +27,7 @@ const ZoomBox = ({ children, className }) => {
           <DialogContent className="max-w-screen h-dvh cursor-zoom-out p-0 m-0 overflow-auto">
             <div className="flex justify-center items-center ">
               {React.cloneElement(children, {
-                className: cn(children.props.className, "max-w-none max-h-none"),
+                className: cn(children.props.className, "max-w-screen max-h-none"),
               })}
             </div>
           </DialogContent>
