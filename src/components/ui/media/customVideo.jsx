411a6575bbc3f3ef5b141e46d62876ec5e487992
Zeke Zhang <sean1832725142@gmail.com>
2024-07-01 07:27:13 +1000

    :sparkles: feat(video): implement new settings for youtube video

--- src/components/ui/media/customVideo.jsx
+++ src/components/ui/media/customVideo.jsx
@@ -5,7 +5,13 @@ import { ConstructYoutubeAltText } from "@/lib/constructAltText";
 const CustomVideo = ({ video }) => {
   return (
     <div className={cn("relative w-full col-span-2 h-[300px] md:h-[600px]", video.className)}>
-      <YoutubeVideo src={video.src} alt={video.alt || ConstructYoutubeAltText(video.src)} />
+      <YoutubeVideo
+        src={video.src}
+        alt={video.alt || ConstructYoutubeAltText(video.src)}
+        isMuted={video.videoSettings?.isMuted || true}
+        isAutoPlay={video.videoSettings?.isAutoPlay || false}
+        startTime={video.videoSettings?.startTime || 0}
+      />
     </div>
   );
 };
--- src/components/ui/media/youtube-video.jsx
+++ src/components/ui/media/youtube-video.jsx
@@ -1,16 +1,27 @@
 import React from "react";
 import { cn } from "@/utils/cn";
+import { SetYoutubeUrl } from "@/lib/youtubeUtil";
+import propTypes from "prop-types";
 
-const YoutubeVideo = ({ src, alt, mute, className }) => {
+const YoutubeVideo = ({ src, alt, className, startTime, isAutoPlay, isMuted }) => {
   return (
     <iframe
-      src={src + (mute ? "&mute=1" : "")}
+      src={SetYoutubeUrl(src, startTime, isAutoPlay, isMuted)}
       title={alt}
       className={cn("absolute inset-0 w-full h-full", className)}
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; web-share"
       allowFullScreen
     />
   );
+};
+
+YoutubeVideo.propTypes = {
+  src: propTypes.string.isRequired,
+  alt: propTypes.string.isRequired,
+  className: propTypes.string,
+  startTime: propTypes.number,
+  isAutoPlay: propTypes.bool,
+  isMuted: propTypes.bool,
 };
 
 export { YoutubeVideo };
